4012fd33ba76a59fb23d2bb9742a881df0c55c3a
Dominik <dominik.zemlik.dev@gmail.com>
2024-01-27 19:31:52 +0100

    feat(task): create controller for crud actions

---
+++ src/task/task.controller.ts
@@ -0,0 +1,217 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  ForbiddenException,
+  Get,
+  NotFoundException,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  Put,
+  Query,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
+import { TaskService } from './task.service';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { UpdateTaskDto } from './dto/update-task.dto';
+import { DatabaseService } from '../database/database.service';
+import { JwtAuthGuard } from '../auth/guard/jwt-auth.guard';
+
+@UseGuards(JwtAuthGuard)
+@Controller('tasks')
+export class TaskController {
+  constructor(
+    private readonly taskService: TaskService,
+    private readonly databaseService: DatabaseService,
+  ) {}
+
+  @Post()
+  async create(
+    @Req() req,
+    @Body() { title, description, subtasks, statusId }: CreateTaskDto,
+  ) {
+    const isUserStatus = await this.taskService.isUserStatus(
+      statusId,
+      req.user.id,
+    );
+
+    if (!isUserStatus) {
+      throw new ForbiddenException('You cannot create task');
+    }
+
+    return this.taskService.create({
+      title,
+      description,
+      subtasks: {
+        createMany: {
+          data: subtasks,
+        },
+      },
+      status: {
+        connect: {
+          id: statusId,
+        },
+      },
+    });
+  }
+
+  @Get()
+  async findMany(
+    @Req() req,
+    @Query('statusId', ParseIntPipe) statusId: number,
+  ) {
+    const isUserStatus = await this.taskService.isUserStatus(
+      statusId,
+      req.user.id,
+    );
+
+    if (!isUserStatus) {
+      throw new ForbiddenException('You cannot read tasks');
+    }
+
+    return this.taskService.findMany({
+      where: {
+        statusId,
+      },
+      select: {
+        id: true,
+        title: true,
+        description: true,
+        statusId: true,
+      },
+      orderBy: {
+        updatedAt: 'asc',
+      },
+    });
+  }
+
+  @Put(':id')
+  async updateTask(
+    @Req() req,
+    @Param('id', ParseIntPipe) id: number,
+    @Body() { title, description, statusId, subtasks }: UpdateTaskDto,
+  ) {
+    const isUserStatus = await this.taskService.isUserStatus(
+      statusId,
+      req.user.id,
+    );
+
+    if (!isUserStatus) {
+      throw new ForbiddenException('You cannot update task');
+    }
+
+    const taskToUpdate = await this.taskService.findUnique({
+      id,
+    });
+
+    if (!taskToUpdate) {
+      throw new NotFoundException('Task not found');
+    }
+
+    await Promise.all([
+      subtasks.map(
+        async (subtask) =>
+          await this.databaseService.subtask.upsert({
+            where: {
+              id: subtask.id,
+            },
+            update: {
+              title: subtask.title,
+            },
+            create: {
+              taskId: taskToUpdate.id,
+              title: subtask.title,
+              isComplete: false,
+            },
+          }),
+      ),
+    ]);
+
+    return this.taskService.update({
+      where: {
+        id,
+      },
+      data: {
+        title,
+        description,
+        status: {
+          connect: {
+            id: statusId,
+          },
+        },
+      },
+      select: {
+        id: true,
+        title: true,
+        description: true,
+        subtasks: {
+          select: {
+            id: true,
+            title: true,
+            isComplete: true,
+          },
+        },
+      },
+    });
+  }
+
+  @Patch(':id')
+  async updateTaskStatus(
+    @Req() req,
+    @Param('id', ParseIntPipe) id: number,
+    @Body() { statusId }: { statusId: number },
+  ) {
+    const taskToUpdate = await this.taskService.findUnique({
+      id,
+    });
+
+    if (!taskToUpdate) {
+      throw new NotFoundException('Task not found');
+    }
+
+    const isUserStatus = await this.taskService.isUserStatus(
+      taskToUpdate.statusId,
+      req.user.id,
+    );
+
+    if (!isUserStatus) {
+      throw new ForbiddenException('You cannot update task');
+    }
+
+    return this.taskService.update({
+      data: {
+        status: {
+          connect: {
+            id: statusId,
+          },
+        },
+      },
+      where: {
+        id,
+      },
+    });
+  }
+
+  @Delete(':id')
+  async delete(@Req() req, @Param('id', ParseIntPipe) id: number) {
+    const taskToRemove = await this.taskService.findUnique({ id });
+
+    if (!taskToRemove) {
+      throw new NotFoundException('Task not found');
+    }
+
+    const isUserStatus = await this.taskService.isUserStatus(
+      taskToRemove.statusId,
+      req.user.id,
+    );
+
+    if (!isUserStatus) {
+      throw new ForbiddenException('You cannot delete task');
+    }
+
+    return this.taskService.delete(taskToRemove);
+  }
+}
--- src/task/task.module.ts
+++ src/task/task.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TaskService } from './task.service';
 import { DatabaseService } from '../database/database.service';
+import { TaskController } from './task.controller';
 
 @Module({
   providers: [TaskService, DatabaseService],
+  controllers: [TaskController],
 })
 export class TaskModule {}
