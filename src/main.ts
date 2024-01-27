3c1f66a63406115dca0ffc1a5137d1cc25f7a3c2
Dominik <dominik.zemlik.dev@gmail.com>
2024-01-27 19:38:10 +0100

    feat(app): configure cors and port

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('PORT');
+
+  const corsOrigin = configService.get<string>(
+    'CORS_ORIGIN',
+    'http://localhost:3000',
+  );
+
+  app.enableCors({
+    origin: corsOrigin,
+    credentials: true,
+  });
+
+  await app.listen(port);
 }
 bootstrap();
